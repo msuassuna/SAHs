54531b1d83019c632f6eee30ce48512d689277f5
msuassuna <msuassuna@gmail.com>
2023-06-15 17:39:45 -0300

    Update GitHub page

--- update.bat
+++ update.bat
@@ -7,14 +7,14 @@ set GITHUB_PASSWORD=Mss!130183
 set GITHUB_REPOSITORY=SAHs
 
 REM Change to the repository directory
-cd %REPO_PATH%
+cd G:\SAHs
 
 REM Update the GitHub page
-%GIT_PATH% add .
-%GIT_PATH% commit -m "Update GitHub page"
-%GIT_PATH% push https://%GITHUB_USERNAME%:%GITHUB_PASSWORD%@github.com/%GITHUB_USERNAME%/%GITHUB_REPOSITORY%.git
+C:\Users\marcus.santos\AppData\Local\Programs\Git\bin\git.exe add .
+C:\Users\marcus.santos\AppData\Local\Programs\Git\bin\git.exe commit -m "Update GitHub page"
+C:\Users\marcus.santos\AppData\Local\Programs\Git\bin\git.exe push https://msuassuna:Mss!130183@github.com/msuassuna/SAHs.git
 
 REM Open Git Bash
-start "" "%GIT_PATH%" bash.exe
+start "" "C:\Users\marcus.santos\AppData\Local\Programs\Git\bin\git.exe" bash.exe
 
 exit
